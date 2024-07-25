fc58b9519475f0353042cd531e850840c051ab52
feryirawan <ir15y4hh@gmail.com>
2024-07-25 16:14:12 +0700

    Oke deh

--- src/components/templates/WebCamPro.jsx
+++ src/components/templates/WebCamPro.jsx
@@ -79,7 +79,7 @@ const WebcamPro = (props) => {
       const video = webcamRef.current.video;
       if (video) {
         if (window.orientation === 45 || window.orientation === -45) {
-          video.style.transform = 'rotate(90deg)';
+          video.style.transform = 'rotate(45deg)';
         } else {
           video.style.transform = 'rotate(0deg)';
         }
